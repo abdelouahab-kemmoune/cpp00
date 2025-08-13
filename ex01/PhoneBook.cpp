21da5feb144180c2c026bd9af919b4ee36ebbb34
Abdelouahab Kemmoune <akemmoun@c3r3p4.1337.ma>
2025-08-13 12:26:01 +0100

    errors

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -14,6 +14,7 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
+#include <limits>
 using std::string;
 
 class Contact{
@@ -81,22 +82,43 @@ public:
 
         std::cout << "First name: ";
         std::getline(std::cin, input);
+        while (input.empty()) {
+            std::cout << "Can't be empty. Enter again: ";
+            std::getline(std::cin, input);
+        }
         c.setFirstName(input);
+
 
         std::cout << "Last name: ";
         std::getline(std::cin, input);
+        while (input.empty()) {
+            std::cout << "Can't be empty. Enter again: ";
+            std::getline(std::cin, input);
+        }
         c.setLastName(input);
 
         std::cout << "Nickname: ";
         std::getline(std::cin, input);
+        while (input.empty()) {
+            std::cout << "Can't be empty. Enter again: ";
+            std::getline(std::cin, input);
+        }
         c.setNickName(input);
 
         std::cout << "Phone number: ";
         std::getline(std::cin, input);
+        while (input.empty()) {
+            std::cout << "Can't be empty. Enter again: ";
+            std::getline(std::cin, input);
+        }
         c.setPhoneNumber(input);
 
         std::cout << "Darkest secret: ";
         std::getline(std::cin,input);
+        while (input.empty()) {
+            std::cout << "Can't be empty. Enter again: ";
+            std::getline(std::cin, input);
+        }
         c.setDarkestSecret(input);
 
         Contacts[Count % 8] = c;
@@ -106,16 +128,46 @@ public:
 public:
     void Search() {
         int total = (Count < 8) ? Count : 8;
-        std::cout << std::setw(10) << "Index" << "|"
-                    << std::setw(10) << "First name" << "|"
-                    << std::setw(10) << "Last name" << "|"
-                    << std::setw(10) << "Nickname" << "|" << std::endl;
-        
-        for (int i = 0; i < total; i++) {
-        std::cout << std::setw(10) << i << "|"
-                    << std::setw(10) << Contacts[i].getFirstName() << "|"
-                    << std::setw(10) << Contacts[i].getLastName() << "|"
-                    << std::setw(10) << Contacts[i].getNickName() << "|" << std::endl;
+        std::cout << std::setw(10) << std::right << "Index" << "|"
+                    << std::setw(10) << std::right << "First name" << "|"
+                    << std::setw(10) << std::right << "Last name" << "|"
+                    << std::setw(10) << std::right << "Nickname" << "|" << std::endl;
+
+        for (int i = 0; i < total ; i++)
+        {
+            std::cout << std::setw(10) << std::right << i << "|";
+            if (Contacts[i].getFirstName().length() > 9)
+                std::cout << std::setw(10) << std::right << Contacts[i].getFirstName().substr(0, 9) + "." << "|";
+            else
+                std::cout << std::setw(10) << std::right << Contacts[i].getFirstName() << "|";
+            if (Contacts[i].getLastName().length() > 9)
+                std::cout << std::setw(10) << std::right << Contacts[i].getLastName().substr(0, 9) + "." << "|";
+            else
+                std::cout << std::setw(10) << std::right << Contacts[i].getLastName() << "|";
+            if (Contacts[i].getNickName().length() > 9)
+                std::cout << std::setw(10) << std::right << Contacts[i].getNickName().substr(0, 9) + "." << "|" << std::endl;
+            else
+                std::cout << std::setw(10) << std::right << Contacts[i].getNickName() << "|" << std::endl;
+        }
+
+
+        int index = 0;
+        std::cout << "Enter the index: ";
+        std::cin >> index;
+        if (std::cin.fail())
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+        std::cout << std::endl;
+
+        if (index <= 8)
+        {
+            if (index >= 0 && index < total) {
+                std::cout << "First name: " << Contacts[index].getFirstName() << std::endl;
+                std::cout << "Last name: " << Contacts[index].getLastName() << std::endl;
+                std::cout << "Nickname: " << Contacts[index].getNickName() << std::endl;
+                std::cout << "Phone number: " << Contacts[index].getPhoneNumber() << std::endl;
+                std::cout << "Darkest secret: " << Contacts[index].getDarkestSecret() << std::endl;
+            }
         }
     }
 };
@@ -128,7 +180,7 @@ int main(int argc, char **argv)
     while (true)
     {
         std::cout << "Enter the command(ADD or SEARCH or EXIT): ";
-        std::getline(std::cin, command);
+        std::getline(std::cin, command); // PROTECT END OF FILE
 
         if (command == "ADD") {
             phoneBook.Add();
@@ -140,6 +192,6 @@ int main(int argc, char **argv)
         else if (command == "EXIT")
             break;
         else
-            std::cout << "unknown command";
+            std::cout << "unknown command" << std::endl;
     }
 }
